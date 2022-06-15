071c9980f0b711871d682ebf4fc06d26f8e580d7
Seth-Rothschild <seth.j.rothschild@gmail.com>
2022-06-14 22:26:15 -0400

    Initial Commit

---
+++ contacts_app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    birthday = models.DateField()
+    notes = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+    notes = models.TextField()
+
+    def __str__(self):
+        return "{} ({})".format(str(self.date), self.description)
