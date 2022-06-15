81349a8902746aa14fbd397fcf4e6271fc1a34b0
Seth-Rothschild <seth.j.rothschild@gmail.com>
2022-06-14 22:38:25 -0400

    Format with black

--- contacts_app/views.py
+++ contacts_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 # Create your views here.
@@ -6,22 +6,21 @@ from django.http import HttpResponse
 from .models import Contact, Event
 from django.template import loader
 
+
 def index(request):
     contacts_list = Contact.objects.all()
-    context = {
-        'contacts_list': contacts_list
-    }
-    template = loader.get_template('contacts_app/index.html')
-    return render(request, 'contacts_app/index.html', context)
+    context = {"contacts_list": contacts_list}
+    template = loader.get_template("contacts_app/index.html")
+    return render(request, "contacts_app/index.html", context)
 
 
 def contact_details(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id)
-    return render(request, 'contacts_app/contact_details.html', {'contact': contact})
+    return render(request, "contacts_app/contact_details.html", {"contact": contact})
 
 
 def event_log(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id)
     events = contact.event_set.all()
     context = {"contact": contact, "events": events}
-    return render(request, 'contacts_app/event_log.html', context)
+    return render(request, "contacts_app/event_log.html", context)
